617ee6a4488bfeb7a662ce1f818a2ff4fa2039aa
Barbara Irons <birons89@gmail.com>
2018-02-11 14:17:55 -0500

    adding more ChangeTest cases

--- src/test/java/VendingMachine/ChangeTest.java
+++ src/test/java/VendingMachine/ChangeTest.java
@@ -23,9 +23,29 @@ public class ChangeTest
 	}
 
 	@Test
-	public void testMakeChange()
+	public void testMakeChange1()
 	{
 		assertEquals("Your change is: 4 quarter(s), 0 dime(s), and 0 nicklel(s).", testChange.makeChange(new BigDecimal("1")));
 	}
+	
+	@Test
+	public void testMakeChangeDollar()
+	{
+		assertEquals("Your change is: 4 quarter(s), 0 dime(s), and 0 nicklel(s).", testChange.makeChange(new BigDecimal("1.00")));
+	}
+	
+	@Test
+	public void testMakeChange40Cents()
+	{
+		assertEquals("Your change is: 1 quarter(s), 1 dime(s), and 1 nicklel(s).", testChange.makeChange(new BigDecimal("0.40")));
+	}
+	
+	@Test
+	public void testMakeChangeNone()
+	{
+		assertEquals("Your change is: 0 quarter(s), 0 dime(s), and 0 nicklel(s).", testChange.makeChange(new BigDecimal("0.00")));
+	}
+	
+	
 	
 }
